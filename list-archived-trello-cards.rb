1397114101ed5f400d2844164cc2fa5d24b5bf35
Chris Roos <chris@seagul.co.uk>
2014-10-07 13:54:04 +0100

    Add script
    Executing the script without any arguments displays a usage notice.

---
+++ list-archived-trello-cards.rb
@@ -0,0 +1,29 @@
+require 'bundler/setup'
+require 'trello'
+
+trello_api_key, trello_app_token, trello_board_id, date_from, date_to = ARGV
+
+date_from = Date.parse(date_from) rescue nil
+date_to = Date.parse(date_to) rescue nil
+
+unless trello_api_key && trello_app_token && trello_board_id && date_from && date_to
+  puts "Usage: #{__FILE__} <trello-developer-token> <trello-application-token> <trello-board-id> <date-from> <date-to>"
+  exit 1
+end
+
+Trello.configure do |config|
+  config.developer_public_key = trello_api_key
+  config.member_token = trello_app_token
+end
+
+board = Trello::Board.find(trello_board_id)
+archived_cards = board.cards(filter: :closed)
+
+archived_cards.sort_by do |card|
+  card.last_activity_date
+end.reverse.reject do |card|
+  card.last_activity_date < date_from or
+  card.last_activity_date > date_to
+end.each do |card|
+  puts "#{card.last_activity_date} - #{card.name}"
+end
