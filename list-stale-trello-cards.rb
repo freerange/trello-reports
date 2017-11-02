32c4179424ad5ad5030cfbcf91c5f62b3245bde4
James Mead <james@floehopper.org>
2017-11-02 09:57:54 +0000

    Rename stale -> neglected to match Harmonia task & instructions

--- list-stale-trello-cards.rb
+++ list-stale-trello-cards.rb
@@ -7,10 +7,10 @@ Dotenv.load
 
 trello_api_key, trello_app_token = ENV.fetch('TRELLO_KEY'), ENV.fetch('TRELLO_TOKEN')
 
-trello_board_id, number_of_weeks_until_considered_stale = ARGV
+trello_board_id, number_of_weeks_until_considered_neglected = ARGV
 
-unless trello_api_key && trello_app_token && trello_board_id && number_of_weeks_until_considered_stale
-  puts "Usage: TRELLO_KEY=<trello-api-key> TRELLO_TOKEN=<trello-token> #{__FILE__} <trello-board-id> <number-of-weeks-until-considered-stale>"
+unless trello_api_key && trello_app_token && trello_board_id && number_of_weeks_until_considered_neglected
+  puts "Usage: TRELLO_KEY=<trello-api-key> TRELLO_TOKEN=<trello-token> #{__FILE__} <trello-board-id> <number-of-weeks-until-considered-neglected>"
   exit 1
 end
 
@@ -19,7 +19,7 @@ Trello.configure do |config|
   config.member_token = trello_app_token
 end
 
-date_before_which_considered_stale = Date.today - (number_of_weeks_until_considered_stale.to_i * 7)
+date_before_which_considered_neglected = Date.today - (number_of_weeks_until_considered_neglected.to_i * 7)
 
 board = Trello::Board.find(trello_board_id)
 open_cards = board.cards(filter: :open)
@@ -27,7 +27,7 @@ open_cards = board.cards(filter: :open)
 open_cards.sort_by do |card|
   card.last_activity_date
 end.reverse.select do |card|
-  card.last_activity_date < date_before_which_considered_stale
+  card.last_activity_date < date_before_which_considered_neglected
 end.each do |card|
   puts "* #{card.last_activity_date} - [#{card.name}](#{card.short_url})"
 end
