7b6a5b7191a0d1fc30494a2bccd639defc366924
James Mead <james@floehopper.org>
2017-11-02 09:54:48 +0000

    Bring list-stale-trello-cards into line with other scripts
    * Specify board ID as command-line arg
    * Specify number of weeks until considered stale as command-line arg
    * Print usage if command-line args not specified
    * Only list stale cards

--- list-stale-trello-cards.rb
+++ list-stale-trello-cards.rb
@@ -7,18 +7,27 @@ Dotenv.load
 
 trello_api_key, trello_app_token = ENV.fetch('TRELLO_KEY'), ENV.fetch('TRELLO_TOKEN')
 
-trello_board_id = 'yTEJzPb8'
+trello_board_id, number_of_weeks_until_considered_stale = ARGV
+
+unless trello_api_key && trello_app_token && trello_board_id && number_of_weeks_until_considered_stale
+  puts "Usage: TRELLO_KEY=<trello-api-key> TRELLO_TOKEN=<trello-token> #{__FILE__} <trello-board-id> <number-of-weeks-until-considered-stale>"
+  exit 1
+end
 
 Trello.configure do |config|
   config.developer_public_key = trello_api_key
   config.member_token = trello_app_token
 end
 
+date_before_which_considered_stale = Date.today - (number_of_weeks_until_considered_stale.to_i * 7)
+
 board = Trello::Board.find(trello_board_id)
 open_cards = board.cards(filter: :open)
 
 open_cards.sort_by do |card|
   card.last_activity_date
-end.reverse.each do |card|
+end.reverse.select do |card|
+  card.last_activity_date < date_before_which_considered_stale
+end.each do |card|
   puts "* #{card.last_activity_date} - [#{card.name}](#{card.short_url})"
 end
